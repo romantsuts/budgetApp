d17b53ee551b07397b3582dacdd14365d5939d84
Roman <33954143+romantsuts@users.noreply.github.com>
2018-09-06 16:11:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var budgetController = (function () {
 
     var Expense = function (id, description, value) {
-        this.id = id;
+        this.id = id; 
         this.description = description;
         this.value = value;
     }
